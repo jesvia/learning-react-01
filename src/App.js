fd1266f501c8cd2deb1441ad4c76cf471c295c6b
jesvia <jesvia.rofl@gmail.com>
2022-12-12 17:07:20 +0400

    added Header.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './Header'; //no need to add .js
+// this Header is a custom element
 function App() {
+  // in this 'return' it looks like html file but it is jsx, it is returning jsx
+  // jsx - js is xml -- resembles html -- not quite the same + allows us to put js expressions
+  // like instead of class there is className
+  // anything simply written in jsx will be rendered as text
+  // but if you use {"x"} now it is rendered as a string with value x
+  // arrays allowed {[1, 2, 3]}
+  // like now it is an expression with a value , CANT RENDER OBJECTS THO
+  // like {{a:1, b:1}} -- will give error
+  // BOOLEANS also not rendered
+  const handleNameChange = () => {
+      const names = ['jess', 'dave', 'alex']
+      const int = Math.floor(Math.random() * 3)
+      return names[int];
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/> 
+      {/* here see, the Header is a custom element */}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+// to create a functional component type rafce after ctrl + alt + r
+// we can uplaod this 'Header' to App.js
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <h1>Groceries List</h1>
+    </header>
+  )
+}
+
+export default Header
